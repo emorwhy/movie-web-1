23c6d567759cf82dd1cf1b72f6bd31526fee0eca
emorwhy y <emoryntucker@gmail.com>
2024-04-11 19:34:33 +0000

    asdf

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "CHANGEME",
+  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
+  VITE_CORS_PROXY_URL: "https://ffmw-proxy.up.railway.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMGViMTM5YTkyOTFkY2FhODZkNjcxMzk2NTgwZjcwMSIsInN1YiI6IjY1YjEzYmY5YTMxNDQwMDE3MjVhYzk0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._V_I-Gpchvq823gqplqo2uq9LfX5TfKNUGBihw0Hx2U",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -11,8 +12,8 @@ window.__CONFIG__ = {
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
   VITE_NORMAL_ROUTER: false,
 
-  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
-  VITE_BACKEND_URL: null,
+  // The backend URL to communicate with
+  VITE_BACKEND_URL: "https://ffmw-backend.up.railway.app",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: ""
